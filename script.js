da2478e484a223dd700baa383e4265bca960343a
CheeseBallad <norcioacizaudric@gmail.com>
2026-06-24 12:12:55 +0000

    Initial app window setup, pill top bar, wallpaper

--- script.js
+++ script.js
@@ -69,3 +69,23 @@ welcomeScreenOpen.addEventListener("click", function() {
 
 //App clicking
 var selectedIcon = undefined
+
+function selectIcon(element) {
+  element.classList.add("selected");
+  selectedIcon = element
+} 
+function deselectIcon(element) {
+  element.classList.remove("selected");
+  selectedIcon = undefined
+}
+function handleIconTap(element) {
+  if (element.classList.contains("selected")) {
+    deselectIcon(element)
+    openWindow(window)
+  } else {
+    selectIcon(element)
+  }
+}
+
+//TUYU
+dragElement(document.querySelector("#TUYU"))
