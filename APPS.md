8b1aa2d213b49077fe6385e4c3057b560aa1e6db
CheeseBallad <norcioacizaudric@gmail.com>
2026-06-25 13:06:41 +0000

    Fix desktop, windows now stack, cleaned JS code

---
+++ APPS.md
@@ -0,0 +1,5 @@
+## Instructions for apps
+
+The do's, dont's, features and workings for the apps
+
+## TUYU
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //windows logic
-dragElement(document.getElementById("welcome")); 
+
+//drag
 function dragElement(element) {
 
   var initialX = 0;
@@ -7,65 +8,62 @@ function dragElement(element) {
   var currentX = 0;
   var currentY = 0;
 
-
   if (document.getElementById(element.id + "header")) {
     document.getElementById(element.id + "header").onmousedown = startDragging;
   } else {
     element.onmousedown = startDragging;
   }
-
- 
   function startDragging(e) {
     e = e || window.event;
     e.preventDefault();
-
     initialX = e.clientX;
     initialY = e.clientY;
-
     document.onmouseup = stopDragging;
     document.onmousemove = dragElement;
   }
 
-  
   function dragElement(e) {
     e = e || window.event;
     e.preventDefault();
-
     currentX = initialX - e.clientX;
     currentY = initialY - e.clientY;
     initialX = e.clientX;
     initialY = e.clientY;
-   
     element.style.top = (element.offsetTop - currentY) + "px";
     element.style.left = (element.offsetLeft - currentX) + "px";
   }
 
- 
   function stopDragging() {
     document.onmouseup = null;
     document.onmousemove = null;
   }
 }
 
-//welcome screen
-var welcomeScreen = document.querySelector("#welcome")
+var biggestIndex = 1;
+var topBar = document.querySelector("#top")
+
+//open and close
 function closeWindow(element) {
   element.style.display = "none"
 }
 function openWindow(element) {
   element.style.display = "flex"
+  element.style.height = "";
+  biggestIndex++;
+  element.style.zIndex = biggestIndex;
+  topBar.style.zIndex = biggestIndex + 1;
 }
-
-var welcomeScreenClose = document.querySelector("#welcomeclose")
-var welcomeScreenOpen = document.querySelector("#welcomeopen")
-
-welcomeScreenClose.addEventListener("click", function() {
-  closeWindow(welcomeScreen);
-});
-
-welcomeScreenOpen.addEventListener("click", function() {
-  openWindow(welcomeScreen);
-});
+function handleWindowTap(element) {
+  biggestIndex++;
+  element.style.zIndex = biggestIndex;
+  topBar.style.zIndex = biggestIndex + 1;
+  deselectIcon(selectedIcon)
+}
+function addWindowTapHandling(element) {
+  element.addEventListener("mousedown", () =>
+    handleWindowTap(element)
+  )
+}
 
 //App clicking
 var selectedIcon = undefined
@@ -87,5 +85,17 @@ function handleIconTap(element) {
   }
 }
 
-//TUYU
-dragElement(document.querySelector("#TUYU"))
+//initalize windows
+function initializeWindow(elementName) {
+  var screen = document.querySelector("#" + elementName);
+  dragElement(screen);
+  addWindowTapHandling(screen);
+
+  var closeBtn = document.querySelector("#" + elementName + "close");
+  var openBtn  = document.querySelector("#" + elementName + "open");
+  if (closeBtn) closeBtn.addEventListener("click", () => closeWindow(screen));
+  if (openBtn)  openBtn.addEventListener("click",  () => openWindow(screen));
+}
+
+initializeWindow("welcome");
+initializeWindow("TUYU");
